3564d5c75f577bf725a07ef6b478402dd5b7052c
Devina <36676408+Devina@users.noreply.github.com>
2018-05-01 20:19:19 +0100

    20 January 2018 Challenge

---
+++ 180120_20Jan/180120_20Jan.js
@@ -0,0 +1,28 @@
+let sections = document.getElementsByClassName('section');
+let progressBar = document.getElementById('nav-progress-bar');
+
+for (let section of sections) {
+  progressBar.innerHTML += `<span class="nav-progress-item"></span>`
+}
+
+let progressDots = document.getElementsByClassName('nav-progress-item');
+
+function isInViewport(element) {
+  var rect = element.getBoundingClientRect();
+  var html = document.documentElement;
+  return (
+    rect.bottom >= (window.innerHeight - 30 || html.clientHeight - 30) &&
+    rect.top < (window.innerHeight || html.clientHeight)
+  );
+}
+
+window.addEventListener("scroll", function() {
+  for (let progressDot of progressDots) {
+    progressDot.className = 'nav-progress-item'
+  }
+  for (let i = 0; i < sections.length; i++) {
+    if (isInViewport(sections[i])) {
+      progressDots[i].className = 'nav-progress-item in-view'
+    }
+  }
+});
